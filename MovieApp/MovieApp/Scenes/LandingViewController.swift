e10c2ffd48aeb2b8d672558a60a5269d71cda166
Gokalp Koksal <gokalp.koksal@accenture.com>
2022-08-05 11:10:11 +0300

    Setup collectionView for landing screen images

--- MovieApp/MovieApp/Scenes/LandingViewController.swift
+++ MovieApp/MovieApp/Scenes/LandingViewController.swift
@@ -7,12 +7,48 @@
 
 import UIKit
 
-// TODO: Implement view elements.
-class LandingViewController: UIViewController, LandingViewModelDelegate {
+// TODO: Fix collection view content
+class LandingViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCollectionView()
+    }
+    
+    private func configureCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        collectionView.collectionViewLayout = layout
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
     }
 
+}
+
+extension LandingViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    // TODO: change number of items with landing image count
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    // TODO: return correct cell with image etc.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+}
+
+extension LandingViewController: UICollectionViewDelegateFlowLayout {
+    
+    // TODO: Fix cells are not ignoring safe areas.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
 }
 
