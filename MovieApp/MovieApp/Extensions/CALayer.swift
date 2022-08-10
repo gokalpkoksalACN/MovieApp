02d349656110f7e5dd0fef22c84973fb34b1e379
Gokalp Koksal <gokalp.koksal@accenture.com>
2022-08-10 12:27:51 +0300

    Add CALayer extension to adjust shadows

---
+++ MovieApp/MovieApp/Extensions/CALayer.swift
@@ -0,0 +1,32 @@
+//
+//  CALayer.swift
+//  MovieApp
+//
+//  Created by gokalp.koksal on 10.08.2022.
+//
+
+import UIKit
+
+extension CALayer {
+    func applySketchShadow(
+        color: UIColor = .black,
+        alpha: Float = 0.5,
+        x: CGFloat = 0,
+        y: CGFloat = 2,
+        blur: CGFloat = 4,
+        spread: CGFloat = 0)
+    {
+        masksToBounds = false
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur / 2.0
+        if spread == 0 {
+            shadowPath = nil
+        } else {
+            let dx = -spread
+            let rect = bounds.insetBy(dx: dx, dy: dx)
+            shadowPath = UIBezierPath(rect: rect).cgPath
+        }
+    }
+}
