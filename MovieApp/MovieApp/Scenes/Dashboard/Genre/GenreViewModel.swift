af72f4fd2c8292cea61f53ad3f0a2f913989eb7d
Gokalp Koksal <gokalp.koksal@accenture.com>
2022-08-12 10:16:34 +0300

    Add helper notify func for genreViewModel

--- MovieApp/MovieApp/Scenes/Dashboard/Genre/GenreViewModel.swift
+++ MovieApp/MovieApp/Scenes/Dashboard/Genre/GenreViewModel.swift
@@ -18,19 +18,23 @@ class GenreViewModel: GenreViewModelProtocol {
     }
     
     func start() {
-        delegate?.handleViewModelOutput(.setLoading(true))
+        notify(.setLoading(true))
         service.getGenreList { [weak self] result in
             guard let self = self else { return }
             switch result {
             case .success(let response):
-                self.delegate?.handleViewModelOutput(.setLoading(false))
+                self.notify(.setLoading(false))
                 let presentations = response.genres.map { GenrePresentation(genre: $0) }
-                self.delegate?.handleViewModelOutput(.updateGenres(presentations))
+                self.notify(.updateGenres(presentations))
             case .failure(let error):
-                self.delegate?.handleViewModelOutput(.setLoading(false))
+                self.notify(.setLoading(false))
                 print(error)
             }
         }
     }
 
+    private func notify(_ output: GenreViewModelOutput) {
+        delegate?.handleViewModelOutput(output)
+    }
+    
 }
