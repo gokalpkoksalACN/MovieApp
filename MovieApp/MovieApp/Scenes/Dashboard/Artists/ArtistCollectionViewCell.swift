e07f44ba4ca4dee88fd062862199384f0c092279
Gokalp Koksal <gokalp.koksal@accenture.com>
2022-08-12 14:35:59 +0300

    Implement custom artist cell for collection view on artists tab for dashboard

---
+++ MovieApp/MovieApp/Scenes/Dashboard/Artists/ArtistCollectionViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ArtistCollectionViewCell.swift
+//  MovieApp
+//
+//  Created by gokalp.koksal on 12.08.2022.
+//
+
+import UIKit
+
+class ArtistCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "ArtistCollectionViewCell"
+    
+    @IBOutlet weak var imageView: UIImageView! {
+        didSet {
+            imageView.contentMode = .scaleAspectFill
+            imageView.backgroundColor = .lightGray
+        }
+    }
+    
+    @IBOutlet weak var nameLabel: UILabel! {
+        didSet {
+            nameLabel.textAlignment = .center
+            nameLabel.font = UIFont(name: "AppleGothic", size: 14)
+            nameLabel.textColor = UIColor(red: 74/255, green: 74/255, blue: 74/255, alpha: 1.0)
+        }
+    }
+}
--- MovieApp/MovieApp/Scenes/Dashboard/Artists/ArtistsViewController.swift
+++ MovieApp/MovieApp/Scenes/Dashboard/Artists/ArtistsViewController.swift
@@ -10,23 +10,32 @@ import UIKit
 // TODO: Add viewModel and service
 class ArtistsViewController: UIViewController {
 
-    @IBOutlet private weak var collectionView: UICollectionView! {
-        didSet {
-            collectionView.delegate = self
-            collectionView.dataSource = self
-        }
-    }
+    @IBOutlet private weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Artists"
+        self.navigationController?.navigationBar.titleTextAttributes = [ NSAttributedString.Key.font: UIFont(name: "AppleGothic", size: 17)!]
+        configureContent()
     }
 
+    private func configureContent() {
+        configureCollectionView()
+    }
+    
+    private func configureCollectionView() {
+        collectionView.backgroundColor = UIColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 238/255)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+    }
 }
 
 extension ArtistsViewController: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         // TODO: Implement
+        print("artist tapped")
     }
     
 }
@@ -40,10 +49,20 @@ extension ArtistsViewController: UICollectionViewDataSource {
     
     // TODO: Fix
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .green
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ArtistCollectionViewCell.identifier, for: indexPath) as! ArtistCollectionViewCell
         return cell
     }
     
+}
+
+extension ArtistsViewController: UICollectionViewDelegateFlowLayout {
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width: CGFloat = (view.frame.width - 40) / 3
+        return CGSize(width: width, height: width + 45)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    }
 }
