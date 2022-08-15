f5f838983e34fb90ed58f9680dd06fb2a82a1892
Gokalp Koksal <gokalp.koksal@accenture.com>
2022-08-15 09:22:05 +0300

    Add tableView for discover tab on dashboard

--- MovieApp/MovieApp/Scenes/Dashboard/Discover/DiscoverViewController.swift
+++ MovieApp/MovieApp/Scenes/Dashboard/Discover/DiscoverViewController.swift
@@ -9,8 +9,36 @@ import UIKit
 
 class DiscoverViewController: UIViewController {
 
+    @IBOutlet private weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+}
+
+extension DiscoverViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // TODO: Implement
+    }
+    
+}
+
+extension DiscoverViewController: UITableViewDataSource {
+    
+    // TODO: Fix
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    // TODO: Fix
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .systemPink
+        return cell
+    }
+    
 }
