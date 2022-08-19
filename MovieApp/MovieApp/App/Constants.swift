8eb69035011ea1719c951666a6d1188d54fcb4df
Gokalp Koksal <gokalp.koksal@accenture.com>
2022-08-19 09:37:00 +0300

    Add app colors as constants

---
+++ MovieApp/MovieApp/App/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  MovieApp
+//
+//  Created by gokalp.koksal on 19.08.2022.
+//
+
+import UIKit
+
+enum AppColors {
+    static let white = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
+    static let tealBlue = UIColor(red: 2/255, green: 148/255, blue: 165/255, alpha: 1.0)
+    static let tealBlue_80 = UIColor(red: 2/255, green: 148/255, blue: 165/255, alpha: 0.8)
+    static let greyishBrown = UIColor(red: 74/255, green: 74/255, blue: 74/255, alpha: 1.0)
+    static let brownGrey = UIColor(red: 155/255, green: 155/255, blue: 155/255, alpha: 1.0)
+    static let veryLightPink = UIColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1.0)
+    static let veryLightPink_65 = UIColor(red: 216/255, green: 216/255, blue: 216/255, alpha: 0.65)
+    static let dark = UIColor(red: 21/255, green: 28/255, blue: 38/255, alpha: 1.0)
+}
